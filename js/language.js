595e6b22db623684db70aa401a158f8abec64ff2
40323230 <daan0014119@gmail.com>
2016-11-11 20:41:37 +0800

    leng

---
+++ js/language.js
@@ -0,0 +1,27 @@
+//all language
+
+//en
+L_english = {
+    nav: [
+        "Home",
+        "Introduction",
+        "About",
+        "Github Repository",
+    ],
+    body: {
+        home: "<h1>Welcome to</h1><h3>Python Solvespace Website!</h3>",
+    }
+}
+
+//tw
+L_taiwan = {
+    nav: [
+        "首頁",
+        "簡介",
+        "關於",
+        "Github 倉儲",
+    ],
+    body: {
+        home: "<h1>歡迎來到</h1><h3>Python Solvespace 網站！</h3>",
+    }
+}
